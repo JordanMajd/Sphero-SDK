8943a721645252b5350d31e239af30251b9206b0
Jordan Majd <jordan.majd@galvanize.com>
2015-12-22 16:48:27 -0700

    update gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -3,17 +3,36 @@
 var gulp = require('gulp');
 var concat = require("gulp-concat");
 var markdox = require("gulp-markdox");
+var del = require('del');
 
 var paths = {
   javascript: 'lib/**/*.js',
-  documentation: 'doc'
+  documentation: 'doc',
+  distribution: 'dist'
 };
 
-// build documentation for all js in ./lib dir
-gulp.task("doc", function(){
-  gulp.del(paths.documentation);
-  gulp.src(paths.javascript)
+// clean documenation then build documentation for all js
+gulp.task("docs",['clean-docs'], function(){
+  return gulp.src(paths.javascript)
     .pipe(markdox())
     .pipe(concat("doc.md"))
     .pipe(gulp.dest(paths.documentation));
 });
+
+// delete existing documentation
+gulp.task('clean-docs', function(){
+  return del(paths.documentation);
+});
+
+// clean current build and then build all js
+gulp.task('build', ['clean'], function(){
+  return gulp.src(paths.javascript)
+    .pipe(gulp.dest(paths.distribution));
+});
+
+// delete existing build
+gulp.task('clean', function(){
+  return del(paths.distribution);
+});
+
+gulp.task('default', ['build']);
