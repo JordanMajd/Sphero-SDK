dd8566a826c312d4bcf49427455c8693b11d3a2a
Jordan Majd <jordan.majd@galvanize.com>
2015-12-23 13:05:27 -0700

    finished writing create and read packe methods

--- app.js
+++ app.js
@@ -14,6 +14,18 @@
 // });
 
 
-var CommandPacket = require('./lib/packet/command-packet');
+var Packet = require('./lib/packet/packet');
+var myPacket = new Packet();
+myPacket.createCommandPacket(0x01, 0x01, 0x52, undefined, true, true);
 
-var myPacket = new CommandPacket(0x00, 0x01, 0x52, undefined, true, true);
+var testResponseBuffer = new Buffer(0x06);
+var data = new Buffer(0x00);
+testResponseBuffer.writeUInt8(0xFF, 0x00);
+testResponseBuffer.writeUInt8(0xFF, 0x01);
+testResponseBuffer.writeUInt8(0x00, 0x02);
+testResponseBuffer.writeUInt8(0x00, 0x03);
+testResponseBuffer.writeUInt8(data.length + 1, 0x04);
+data.copy(testResponseBuffer, 0x05);
+testResponseBuffer.writeUInt8(myPacket.computeCheckSum(testResponseBuffer), data.length + 0x05);
+
+myPacket.readResponsePacket(testResponseBuffer);
