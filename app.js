4ec8f7851a709991b6dcc01b62ed6cd9e7006da8
Jordan Majd <jordan.majd@galvanize.com>
2015-12-25 18:25:08 -0800

    added convenience api list and services list, test case for changing rgb color

--- app.js
+++ app.js
@@ -1,31 +1,39 @@
 'use strict';
 
-// var NobleConnection = require('./lib/connection/noble-connection');
+var NobleConnection = require('./lib/connection/noble-connection');
+var CommandPacket = require('./lib/packet/command-packet');
+var api = require('./lib/api/api');
+var servicesList = require('./lib/utility/services-list');
+
+var nobleConn = new NobleConnection();
+
+nobleConn.connect('3ce5a3fa5fef4aeebe2c7858f8d8de25', function(){
+  console.log('ready');
+  sendTestPacket();
+});
+
+
+// var Packet = require('./lib/packet/packet');
+// var myPacket = new Packet();
+// myPacket.createCommandPacket(0x01, 0x01, 0x52, undefined, true, true);
 //
-// var nobleConn = new NobleConnection();
-// // nobleConn.startScan();
-// //
-// // setTimeout(function(){
-// //   console.log('stopScan');
-// //   nobleConn.stopScan();
-// // },4000);
-// nobleConn.connect('3ce5a3fa5fef4aeebe2c7858f8d8de25', function(){
-//   console.log('ready');
-// });
+// var testResponseBuffer = new Buffer(0x06);
+// var data = new Buffer(0x00);
+// testResponseBuffer.writeUInt8(0xFF, 0x00);
+// testResponseBuffer.writeUInt8(0xFF, 0x01);
+// testResponseBuffer.writeUInt8(0x00, 0x02);
+// testResponseBuffer.writeUInt8(0x00, 0x03);
+// testResponseBuffer.writeUInt8(data.length + 1, 0x04);
+// data.copy(testResponseBuffer, 0x05);
+// testResponseBuffer.writeUInt8(myPacket.computeCheckSum(testResponseBuffer), data.length + 0x05);
+//
+// myPacket.readResponsePacket(testResponseBuffer);
 
-
-var Packet = require('./lib/packet/packet');
-var myPacket = new Packet();
-myPacket.createCommandPacket(0x01, 0x01, 0x52, undefined, true, true);
-
-var testResponseBuffer = new Buffer(0x06);
-var data = new Buffer(0x00);
-testResponseBuffer.writeUInt8(0xFF, 0x00);
-testResponseBuffer.writeUInt8(0xFF, 0x01);
-testResponseBuffer.writeUInt8(0x00, 0x02);
-testResponseBuffer.writeUInt8(0x00, 0x03);
-testResponseBuffer.writeUInt8(data.length + 1, 0x04);
-data.copy(testResponseBuffer, 0x05);
-testResponseBuffer.writeUInt8(myPacket.computeCheckSum(testResponseBuffer), data.length + 0x05);
-
-myPacket.readResponsePacket(testResponseBuffer);
+function sendTestPacket(){
+  var data = new Buffer(0);
+  data.writeUInt8(0xFF);
+  data.writeUInt8(0xFF);
+  data.writeUInt8(0xFF);
+  var rollPacket = new CommandPacket(0x02, 0x20, 0x00, data, false, false);
+  nobleConn.writeCharectaristic('22bb746f2ba075542d6f726568705327', '22bb746f2ba175542d6f726568705327', rollPacket);
+}
