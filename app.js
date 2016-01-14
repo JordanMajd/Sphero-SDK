70b609672802d579184f5d1c754bf8b1298cfc19
Jordan Majd <jordan.majd@galvanize.com>
2016-01-14 11:59:28 -0700

    made tweaks to notify internals

--- app.js
+++ app.js
@@ -1,62 +1,7 @@
-// 'use strict';
-//
-// var NobleConnection = require('./lib/connection/noble-connection');
-// var CommandPacket = require('./lib/packet/command-packet');
-// // var api = require('./lib/api/api');
-// var servicesList = require('./lib/utility/services-list');
-// //
-// // var nobleConn = new NobleConnection();
-// //
-// // nobleConn.connect('3ce5a3fa5fef4aeebe2c7858f8d8de25', function(){
-// //   console.log('ready');
-// //   setTimeout(sendTestPacket, 4000);
-// //   // sendTestPacket();
-// // });
-// //
-// //
-// // // var Packet = require('./lib/packet/packet');
-// // var myPacket = new Packet();
-// // myPacket.createCommandPacket(0x01, 0x01, 0x52, undefined, true, true);
-// //
-// // var testResponseBuffer = new Buffer(0x06);
-// // var data = new Buffer(0x00);
-// // testResponseBuffer.writeUInt8(0xFF, 0x00);
-// // testResponseBuffer.writeUInt8(0xFF, 0x01);
-// // testResponseBuffer.writeUInt8(0x00, 0x02);
-// // testResponseBuffer.writeUInt8(0x00, 0x03);
-// // testResponseBuffer.writeUInt8(data.length + 1, 0x04);
-// // data.copy(testResponseBuffer, 0x05);
-// // testResponseBuffer.writeUInt8(myPacket.computeCheckSum(testResponseBuffer), data.length + 0x05);
-// //
-// // myPacket.readResponsePacket(testResponseBuffer);
-//
-// // function sendTestPacket(){
-//   // var data = new Buffer(3);
-//   // data.writeUInt8(0xFF, 0x00);
-//   // data.writeUInt8(0xFF, 0x01);
-//   // data.writeUInt8(0xFF, 0x02);
-//   // var rollPacket = new CommandPacket(0x00, 0x11, 0x00, undefined, true, true);
-//   // nobleConn.writeCharectaristic('22bb746f2ba075542d6f726568705327', '22bb746f2ba175542d6f726568705327', rollPacket.packetBuffer,function(){
-//   // var testBuff = new Buffer(1);
-//   // testBuff.writeUInt8(0x01);
-//   // nobleConn.writeCharectaristic('22bb746f2bb075542d6f726568705327', '22bb746f2bbf75542d6f726568705327', testBuff,function(){
-//     // console.log('changed color');
-//   // });
-// // }
-//
-// let CoreCmd = require('./lib/command/core-command');
-// // var command = Object.getPrototypeOf(new CoreCmd());
-// // let cmd = new CoreCmd();
-//
-// let cmd = Object.getPrototypeOf(new CoreCmd());
-// let newObj = {};
-// for(let prop in cmd){
-//   newObj[prop] = cmd[prop];
-// }
-// console.log(newObj);
-// newObj.ping();
-
 'use strict';
+
+//NOTE: maybe packets are corrupt
+//NOTE: maybe I need to cache services and characteristics after I've awakened it.
 
 const BB8 = require('./lib/orb/bb8-orb');
 
@@ -64,19 +9,9 @@ let myBB8 = new BB8('3ce5a3fa5fef4aeebe2c7858f8d8de25');
 
 myBB8.connection.connect()
   .then( () => {
-    console.log('It worked!');
+    console.log('Connected to BB8.');
     return myBB8.command.setDevMode();
   })
   .then( () => {
-    console.log('Dev mode enabled...');
-    // return myBB8.command.roll();
-    // myBB8.connection.writeCharectaristic('22bb746f2bb075542d6f726568705327', '22bb746f2bbf75542d6f726568705327', new Buffer(255),function(){
-      // console.log('changed color');
-    // });
+    console.log('Dev mode enabled.');
   });
-  // .then( () => {
-  //   console.log('Should be rolling...');
-  // })
-  // .catch( (error) => {
-  //   console.error(error.stack);
-  // });
