fc35f31adc8af259d412c0488284010e441d1484
Jordan Majd <jordan.majd@galvanize.com>
2016-01-14 17:11:24 -0700

    FIXED. WE HAVE ACTIVE COMMANDS

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 //NOTE: maybe packets are corrupt
 //NOTE: maybe I need to cache services and characteristics after I've awakened it.
-
+//TODO: md5 is fuckd
 const BB8 = require('./lib/orb/bb8-orb');
 
 let myBB8 = new BB8('3ce5a3fa5fef4aeebe2c7858f8d8de25');
