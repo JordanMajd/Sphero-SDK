c0155fa8340c27b2c4a58b2b8a954c657c9e975f
Jordan Majd <jordan.majd@galvanize.com>
2016-05-19 10:51:15 -0600

    begins core-command-spec implementation, formats noble-connection, updates deps, fixes pathing issues

--- spec/command/command-spec.js
+++ spec/command/command-spec.js
@@ -6,5 +6,37 @@
 
 'use strict';
 
+const BB8 = require('../../lib/sphero-sdk').BB8;
 
-// const Command = require('../../lib/command/command');
+describe('command', function() {
+
+  let pingBB8;
+
+  beforeEach(function(done) {
+    pingBB8 = new BB8('3ce5a3fa5fef4aeebe2c7858f8d8de25');
+    pingBB8.connection.connect().then(function() {
+      done();
+    });
+  });
+
+  afterEach(function(done){
+    pingBB8.connection.disconnect().then(function() {
+      done();
+    });
+  });
+
+
+  it('pings the device and the device responds', function(done) {
+
+    pingBB8.command.ping()
+      .then(function() {
+        expect(true).toBe(true);
+        done();
+      })
+      .catch(function() {
+        expect(true).toBe(false);
+        done();
+      });
+
+  });
+});
